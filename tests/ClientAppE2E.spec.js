d2618440087dc24b746def52cbd0ff985d6380a3
Rv13s <ravirajawork@gmail.com>
2026-01-28 00:25:08 +0530

    add to cart to payement page

--- tests/ClientAppE2E.spec.js
+++ tests/ClientAppE2E.spec.js
@@ -11,16 +11,18 @@ test.only('Add to cart test', async({page})=>{
     await emailIDField.fill('testra@gmail.com');
     await passwordField.fill("Test@123");
     await loginBtn.click();
+    await page.waitForLoadState('networkidle')
+    //await page.locator('.cart-body').first().waitFor();
     //Assertion
-    const pageUrlAssert = await expect(page).toHaveURL('https://rahulshettyacademy.com/client/#/dashboard/dash');
-    const crntUrl = page.url();
-    console.log('pageUrlAssert : '+pageUrlAssert); 
-    console.log('Current Url : '+crntUrl);
+    //const pageUrlAssert = await expect(page).toHaveURL('https://rahulshettyacademy.com/client/#/dashboard/dash');
+    //const crntUrl = page.url();
+    // console.log('pageUrlAssert : '+pageUrlAssert); 
+    //console.log('Current Url : '+crntUrl);
     //const allProductTitle = await page.locator(".card-body b").first().allTextContents();
     //console.log('All product title: '+allProductTitle);
     const allProductCount = await allProducts.count();
     console.log(allProductCount)
-    //await page.pause();
+    //await page.pause();  
 
     for(let i = 0 ; i <allProductCount ; i++){
         if (await allProducts.nth(i).locator('b').textContent() ===  prod ){
@@ -37,5 +39,21 @@ test.only('Add to cart test', async({page})=>{
     const isprd = await page.locator(`h3:has-text('${prod}')`).isVisible();
     console.log("Is cart prd visible: " + isprd)
     console.log(expect(isprd).toBeTruthy());
+
+    //click checkout
+    await page.locator('text=Checkout').click();
+    await page.locator('input[placeholder="Select Country"]').pressSequentially('ind', {delay:150});
+
+    const dropdownOptions = page.locator('.ta-results');
+     await dropdownOptions.waitFor();
+    const optionsCount = await dropdownOptions.count();
+    for(let i =0; i<optionsCount;i++){
+       const text = await dropdownOptions.locator('button').nth(i).textContent();
+       if(text.trim()===" India"){
+        await dropdownOptions.locator("buttton").nth(i).click();
+        break;
+       }
+    }
+
     await page.pause();
 })
